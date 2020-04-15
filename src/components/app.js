6c38b552a90d6b557e87f38435950701c5361f21
Gamygams1234 <gamyburgos@gmail.com>
2020-04-15 13:22:45 -0700

    Implementing a catch all in the Router

--- src/components/app.js
+++ src/components/app.js
@@ -9,6 +9,7 @@ import Home from "./pages/home";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -24,7 +25,8 @@ export default class App extends Component {
               <Route path="/about-me" component={About}></Route>
               <Route path="/contact" component={Contact}></Route>
               <Route path="/blog" component={Blog}></Route>
-              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
