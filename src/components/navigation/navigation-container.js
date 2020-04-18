f1cf7f1177f45ed9245fbace8a525b6e435fda87
Gamygams1234 <gamyburgos@gmail.com>
2020-04-17 17:25:54 -0700

    Starting the Styles, adding our font, using flexbox for our nav ba

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -4,15 +4,18 @@ import { NavLink } from "react-router-dom";
 export default class NavigationContainer extends Component {
   render() {
     return (
-      <div>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        <NavLink to="/about-me">About</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/about-me">About</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
 
-        {false ? <button>Add Blog</button> : null}
+          {false ? <button>Add Blog</button> : null}
+        </div>
+        <div className="right-side">GAMALIEL BURGOS</div>
       </div>
     );
   }
