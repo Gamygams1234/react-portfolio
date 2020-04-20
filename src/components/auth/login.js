d66cc6949a79efe963dca43eae3e5a665d397e1f
Gamygams1234 <gamyburgos@gmail.com>
2020-04-19 22:00:41 -0700

    Creating initial login component by changing state, Creating authentication with react

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    // we are creatins a session on the server
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log("response", response);
+      });
+    e.preventDefault();
+    console.log("Handle submit", this.state.email, this.state.password);
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
   render() {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-        <form>
-          <input type="text" />
-          <input type="password" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="email" name="email" placeholder="Your email" value={this.state.email} onChange={this.handleChange} />
+          <input type="password" name="password" placeholder="Your password" value={this.state.password} onChange={this.handleChange} />
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </form>
       </div>
     );
