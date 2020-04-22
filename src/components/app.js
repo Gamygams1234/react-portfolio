5a23e9f1e354e2eb1b611016dadf053d7c775035
Gamygams1234 <gamyburgos@gmail.com>
2020-04-22 14:35:14 -0700

    Introduction to Render Props in React

--- src/components/app.js
+++ src/components/app.js
@@ -12,6 +12,23 @@ import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  // creating state indication logged in and rendering it to auth
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+    };
+  }
+  handleSuccessfulLogin = (e) => {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+    });
+  };
+  handleUnsuccessfulLogin = (e) => {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+    });
+  };
   render() {
     return (
       <div className="container">
@@ -21,7 +38,7 @@ export default class App extends Component {
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home}></Route>
-              <Route path="/auth" component={Auth}></Route>
+              <Route path="/auth" render={(props) => <Auth {...props} handleSuccessfulLogin={this.handleSuccessfulLogin} handleUnsuccessfulLogin={this.handleUnsuccessfulLogin} />}></Route>
               <Route path="/about-me" component={About}></Route>
               <Route path="/contact" component={Contact}></Route>
               <Route path="/blog" component={Blog}></Route>
