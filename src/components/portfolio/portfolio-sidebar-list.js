5fce02e337ec9072f141075d29108cd9d66bf940
Gamygams1234 <gamyburgos@gmail.com>
2020-04-23 12:06:34 -0700

    Finalizing Portfolio Manager Sidebar List Styles

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -5,15 +5,15 @@ const PortfolioSidebarList = (props) => {
   // mapping over the list
   const portfolioList = props.data.map((item) => {
     return (
-      <div>
-        <div>
+      <div className="portfolio-item-thumb">
+        <div className="portfolio-thumb-image">
           <img src={item.thumb_image_url} />
         </div>
-        <h1>{item.name}</h1>
+        <h1 className="title">{item.name}</h1>
         <h2>{item.id}</h2>
       </div>
     );
   });
-  return <div>{portfolioList}</div>;
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
 };
 export default PortfolioSidebarList;
