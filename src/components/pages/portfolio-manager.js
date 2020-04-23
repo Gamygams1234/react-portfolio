bf77b9ed2c7937c2b50e5441744763bf76308785
Gamygams1234 <gamyburgos@gmail.com>
2020-04-23 11:16:06 -0700

    Populating the Portfolio Manager State with API Call Data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+    this.state = {
+      portfolioItems: [],
+    };
+  }
+  getPortfolioItems() {
+    axios
+      .get("https://gamyburgos.devcamp.space/portfolio/portfolio_items", { withCredentials: true })
+      .then((response) => {
+        this.setState({
+          // in order to get an array in the state, we want to use a spread operator
+          portfolioItems: [...response.data.portfolio_items],
+        });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
+  }
+  componentDidMount() {
+    this.getPortfolioItems(); // putting the getPortfolioItems in component did mount
+    // we want to get it ath the start nd load it once
+  }
   render() {
     return (
       <div className="portfolio-manager-wrapper">
