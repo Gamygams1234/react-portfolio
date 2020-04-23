36dd801b9defb19b6e61e1d20078d9552d8bd020
Gamygams1234 <gamyburgos@gmail.com>
2020-04-22 21:15:40 -0700

    Finalizing the Logout Functionality with a Higher Order Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { withRouter } from "react-router";
 
-export default function NavigationContatiner(props) {
+const NavigationContatiner = (props) => {
   {
     /*this dynamic Link will help us with making sure that we can put this link when  we are logged in  */
   }
@@ -13,6 +15,24 @@ export default function NavigationContatiner(props) {
         </NavLink>
       </div>
     );
+  };
+
+  const handleSignOut = () => {
+    axios
+      .delete("https://api.devcamp.space/logout", { withCredentials: true })
+      .then((response) => {
+        // this will take us back to the home when we sign out
+        if (response.status === 200) {
+          // this will only work if we have a higher order component
+          props.history.push("/");
+          props.handleSuccessfulLogout();
+        }
+        // it is proper to return something after a promise
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("Error signing out", error);
+      });
   };
 
   return (
@@ -37,7 +57,13 @@ export default function NavigationContatiner(props) {
         {/*using a ternary operator to see if we are logged in or not */}
         {props.loggedInStatus === "LOGGED_IN" ? dynamicLink("/blog", "Blog") : null}
       </div>
-      <div className="right-side">GAMALIEL BURGOS</div>
+      <div className="right-side">
+        GAMALIEL BURGOS
+        {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}> Sign Out</a> : null}
+      </div>
     </div>
   );
-}
+};
+// since we are in a functional component, we don't need to use the THIS keyword in our JSX
+
+export default withRouter(NavigationContatiner); // this is where we wrap the higher order component
