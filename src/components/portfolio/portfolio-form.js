295b0964581621f90953992ab54bb57903e94d28
Gamygams1234 <gamyburgos@gmail.com>
2020-04-24 20:34:31 -0700

    Creating a Form Input Mixin

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -92,34 +92,30 @@ export default class PortfolioForm extends Component {
   };
   render() {
     return (
-      <div>
-        <h1>PortfolioForm</h1>
-
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <input type="text" name="name" placeholder="Portfolio Item Name" value={this.state.name} onChange={this.handleChange} />
-            <input type="text" name="url" placeholder="URL" value={this.state.url} onChange={this.handleChange} />
-            <input type="text" name="position" placeholder="Position" value={this.state.position} onChange={this.handleChange} />
-            <select type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleChange}>
-              {/* making a select tag with the options */}
-              <option value="eCommerce">eCommerce</option>
-              <option value="Scheduling">Scheduling</option>
-              <option value="Enterprise">Enterprise</option>
-            </select>
-          </div>
-          <div>
-            <textarea type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleChange} />
-          </div>
-          <div className="image-uploaders">
-            <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleThumbDrop()} ref={this.thumbRef}></DropzoneComponent>
-            <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleBannerDrop()} ref={this.bannerRef}></DropzoneComponent>
-            <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleLogoDrop()} ref={this.logoRef}></DropzoneComponent>
-          </div>
-          <div>
-            <button type="submit">Save</button>
-          </div>
-        </form>
-      </div>
+      <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
+        <div>
+          <input type="text" name="name" placeholder="Portfolio Item Name" value={this.state.name} onChange={this.handleChange} />
+          <input type="text" name="url" placeholder="URL" value={this.state.url} onChange={this.handleChange} />
+          <input type="text" name="position" placeholder="Position" value={this.state.position} onChange={this.handleChange} />
+          <select type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.handleChange}>
+            {/* making a select tag with the options */}
+            <option value="eCommerce">eCommerce</option>
+            <option value="Scheduling">Scheduling</option>
+            <option value="Enterprise">Enterprise</option>
+          </select>
+        </div>
+        <div>
+          <textarea type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.handleChange} />
+        </div>
+        <div className="image-uploaders">
+          <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleThumbDrop()} ref={this.thumbRef}></DropzoneComponent>
+          <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleBannerDrop()} ref={this.bannerRef}></DropzoneComponent>
+          <DropzoneComponent config={this.componentConfig()} djsConfig={this.djsConfig()} eventHandlers={this.handleLogoDrop()} ref={this.logoRef}></DropzoneComponent>
+        </div>
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
     );
   }
 }
