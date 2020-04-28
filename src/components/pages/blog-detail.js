a35b66237f026af035e7427547ece7580b54af05
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 15:04:42 -0700

    Calling a Single Blog Item from the API in React

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+    };
+  }
+  getBlogItem() {
+    axios
+      .get(`https://gamyburgos.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`)
+      .then((res) => {
+        console.log("response", res);
+      })
+      .catch((error) => {
+        console.log("get Item Error", error);
+      });
+  }
+  componentDidMount() {
+    this.getBlogItem();
+  }
   render() {
+    console.log("currentId", this.state.currentId);
     return (
       <div>
         <h1>John Cena Sucks</h1>
