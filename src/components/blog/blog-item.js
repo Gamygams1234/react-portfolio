23ea0940c28b125c56c056f3e83e35baec96cda8
Gamygams1234 <gamyburgos@gmail.com>
2020-04-28 15:33:20 -0700

    Applying Styles to the Blog Detail Component

--- src/components/blog/blog-item.js
+++ src/components/blog/blog-item.js
@@ -5,11 +5,12 @@ const BlogItem = (props) => {
   //here we are doing destruturing to pull in everything
   const { id, blog_status, content, title, featured_image_url } = props.blogItem;
   return (
-    <div>
-      <Link to={`/b/${id}`}>
+    <div className="blog-container">
+      <div className="content-container">
         <h1>{title}</h1>
-      </Link>
-      <div>{content}</div>
+        <img src={featured_image_url} />
+        <div>{content}</div>
+      </div>
     </div>
   );
 };
