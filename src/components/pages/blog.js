cf56fe27840fce89057b46bfe894d72c868f3db8
Gamygams1234 <gamyburgos@gmail.com>
2020-04-27 21:35:57 -0700

    Calling the Blog API and Storing the Data in State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 // making a class component
 class Blog extends Component {
   constructor() {
     super();
+    this.state = {
+      blogItems: [],
+    };
+  }
+  getBlogItems = () => {
+    axios
+      .get("https://gamyburgos.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true })
+      .then((response) => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItems error", error);
+      });
+  };
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
